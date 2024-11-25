498071fc89f8929f1d6fe6b5d27a1f23b61425aa
pablomesq <pablo_omesquita@hotmail.com>
2024-11-25 17:17:28 -0300

    teste de deploy alteração código

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo com deploy com Kubernetes - 2.0")
+	fmt.Fprintf(w, "App runner Digital Ocean with Kubernetes")
 }
 
 func main() {
